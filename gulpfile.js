9048e9e7f43a6c80ccbf78ff409a20dbdbf39f1e
Daniel Paschke <paschdan@gmail.com>
2017-06-08 14:16:17 +0200

    add publish gulp job

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
+var s3Config = {
+  bucket: 'paschdan-miro',
+  region: 'eu-central-1'
+}
+var s3 = require('gulp-s3');
+var awspublish = require('gulp-awspublish');
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -85,6 +91,46 @@ gulp.task('browserSync', function() {
             baseDir: ''
         }
     })
+});
+
+gulp.task('publish', function() {
+
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  var publisher = awspublish.create({
+    region: 'eu-central-1',
+    params: {
+      Bucket: 'paschdan-miro'
+    }
+  }, {
+    cacheFileName: 'aws.cache'
+  });
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+    // ...
+  };
+
+  return gulp.src([
+    'index.html',
+    'js/creative.min.js',
+    'css/creative.min.css',
+    'img/**/*',
+    'vendor/**/*'
+  ], { "base" : "." })
+    .pipe(rename(function (path) {
+      // This is weird, but is needed to make the file use the relative path...
+    }))
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+    // print upload updates to console
+    .pipe(awspublish.reporter());
 });
 
 // Dev task with browserSync
