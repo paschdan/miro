1454e39c0c3e4412aaa27dd11da540834ee50437
Daniel Paschke <paschdan@gmail.com>
2017-06-08 17:22:33 +0200

    concat vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 var awspublish = require('gulp-awspublish');
+var concat = require('gulp-concat');
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -51,33 +52,29 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'));
+gulp.task('vendor', function() {
+  gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/scrollreveal/dist/scrollreveal.min.js'])
+    .pipe(uglify())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('vendor/js/'));
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'));
+  gulp.src([
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'node_modules/magnific-popup/dist/magnific-popup.css'])
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('vendor/css/'));
 
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'));
-
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'));
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+  gulp.src(['node_modules/font-awesome/fonts/**'])
+    .pipe(gulp.dest('vendor/fonts'))
 });
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'vendor']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
